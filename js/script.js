b0d4d8960fcfc023ca9d6d6ffa8d7b7c85efec6a
DestinTech <destintech@gmail.com>
2020-08-02 18:54:33 -0600

    isolated variables in modules and classes to follow clean code guidelines.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,55 @@
-let players = [];
-let playerCounter = 0;
+const playerCounter = (() => {
+    let count = 0;
+        /* Putting the count here inside the module ensures it's created,
+            and gives access to the functions inside of it. 
+    */
+    const getNewID = () =>{
+        return count++; //return the count, and increment it
+    };
+    return {getNewID};
+})();
+
+class Player{    
+    //creates players and sets ID's and markers
+    constructor(name){
+
+        this.playerID = playerCounter.getNewID();   //gets a unique player ID        
+        this.name = name;
+        this.marker = this.getMarker(); 
+        }
+
+    getMarker = () => {
+            if (this.playerID == 0){
+                return "X";
+            } 
+            else if (this.playerID == 1){
+                return "O";
+            }
+    
+    }
+}
 
 const gameBoard = (() => {
 // This is a module, it will automatically run when the script is loaded,
-// creating and executing what is inside of it.
+// creating and executing what is inside of it. any expored functions can be accessed outside of the scope.
     let board = {};
 
 })();
-const Player = {
-}
-
-const playerFactory = (name) => {
-    this.name = name;
-    this.marker = (()=>{
-        console.log( {playerCounter});
-        if (playerCounter == 0){
-            playerCounter =1;
-            return "X";
-
-        }
-        if (playerCounter == 1){
-            return "O";
-        }
-        else return `error: playerCounter = ${playerCounter}`;
-    })();
-    return {name, marker};
-}
 
 const displayController =(() => {
+    let players = [];
 // Keep all the logic related to the enviroment here
 
     for (i = 1; i<3; i++){ //gets the name for 2 players, adds to array.
-        players.push(playerFactory(window.prompt("What is your name?", `player ${i}`)));    
+        players.push(new Player(window.prompt("What is your name?", `player ${i}`)));    
     }   
 // now that we have the players made
     console.log(players);
 
+/*    let paul = new Player("paul");
+    let pul = new Player("pul");
 
+    console.log(paul);
+    console.log(pul);
+    */
 })();
